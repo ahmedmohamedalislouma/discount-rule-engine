efcf988b935c5d9232698e9f903408b00f1ad4a5
Ahmed Slouma <101593935+ahmedmohamedalislouma@users.noreply.github.com>
2023-03-30 19:41:37 +0200

    Update DiscountRuleEngine.scala

--- DiscountRuleEngine.scala
+++ DiscountRuleEngine.scala
@@ -70,7 +70,7 @@ object RuleEngine extends App {
     })
 
 
-    val daysRemainingDiscount = Map((0 to 29).map(day => day -> (1.0 - day )* 0.01).reverse: _*)
+    val daysRemainingDiscount = Map((0 to 29).map(day => day -> ((30 - day) * 0.01)*100).reverse: _*)
 
 
     // Define a function to calculate the days remaining for expiry
